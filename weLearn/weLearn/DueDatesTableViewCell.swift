1aefc4aeb14965b787d6b1798ec192f6e863d0fc
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-03-03 16:32:19 -0500

    clock cell now also constrained to contentview & has two fields, for timer and assignment

--- weLearn/weLearn/DueDatesTableViewCell.swift
+++ weLearn/weLearn/DueDatesTableViewCell.swift
@@ -28,19 +28,45 @@ class DueDatesTableViewCell: UITableViewCell {
     }
 
     func setupHierarchy() {
-        self.contentView.addSubview(label)
+        self.contentView.addSubview(timerLabel)
+        self.contentView.addSubview(assignmentLabel)
     }
     
     func setupConstraints() {
-        label.snp.makeConstraints { (lbl) in
-            lbl.leading.equalToSuperview().offset(10)
-            lbl.trailing.equalToSuperview().inset(10)
-            lbl.centerY.equalToSuperview()
+        timerLabel.snp.makeConstraints { (lbl) in
+            lbl.leading.equalTo(contentView).offset(10)
+            lbl.trailing.equalTo(contentView).inset(10)
+            lbl.top.equalTo(contentView).offset(10)
+            lbl.bottom.equalTo(assignmentLabel.snp.top).inset(-10)
+        }
+        
+        assignmentLabel.snp.makeConstraints { (lbl) in
+            lbl.leading.equalTo(contentView).offset(10)
+            lbl.trailing.equalTo(contentView).inset(10)
+            lbl.bottom.equalTo(contentView).inset(10)
         }
     }
 
-    lazy var label: UILabel = {
+    lazy var timerLabel: UILabel = {
         let lbl = UILabel()
+        lbl.font = UIFont(name: "Thirtysix", size: 36)
+        lbl.textColor = UIColor.weLearnGreen
+        lbl.textAlignment = .center
+        lbl.layer.shadowColor = UIColor.weLearnBlack.cgColor
+        lbl.layer.shadowOffset = CGSize(width: -2, height: 3)
+        lbl.layer.shadowOpacity = 3
+        lbl.layer.shadowRadius = 1
+        lbl.layer.masksToBounds = false
+        lbl.numberOfLines = 2
+        return lbl
+    }()
+    
+    lazy var assignmentLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.font = UIFont(name: "Avenir-Black", size: 24)
+        lbl.textColor = UIColor.weLearnGreen
+        lbl.textAlignment = .center
+        lbl.layer.masksToBounds = false
         return lbl
     }()
 }
--- weLearn/weLearn/HomeViewController.swift
+++ weLearn/weLearn/HomeViewController.swift
@@ -139,7 +139,7 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
 //    }
     
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        return 15
+        return 20
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -152,7 +152,7 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
             cell = tableView.dequeueReusableCell(withIdentifier: announcementCellID, for: indexPath)
             if let firstCell = cell as? AnnouncementTableViewCell {
                 firstCell.date.text = self.title
-                firstCell.quote.text = "You all got A's! Wow! adsf fw f wf qw  we fgwg eg w  w 4g weg  eg wa g we  weg w e4 w4 tg 43g  vw ww"
+                firstCell.quote.text = "You all got A's! Wow! adsf fw f wf qw  we fgwg eg w  w 4g weg  eg wa g we  weg w e4 w4 tg 43g  vw ww gfhtftydtyc  tyfytfutfy yftfytf ytc yyt fyft ft ydydh fyjf uttuyk  yfyfitycfgccjcjf        fdgfdtxxgc  ytfy f  yt        c."
                 firstCell.author.text = "- Ben"
             }
             
@@ -168,7 +168,8 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         case 2:
             cell = tableView.dequeueReusableCell(withIdentifier: dueDatesCellID, for: indexPath)
             if let thirdCell = cell as? DueDatesTableViewCell {
-                thirdCell.label.text = "7 days until midterm..."
+                thirdCell.timerLabel.text = "7 days"
+                thirdCell.assignmentLabel.text = "until midterm..."
                 // MARK: - Dummy timer runs down here!
                 
                 /* Remove the upcoming pair of stars and slashes on this line to comment out dummy timer */
@@ -183,20 +184,12 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
                     let hours = Int(self.timeInSeconds) / 3600 % 24
                     let minutes = Int(self.timeInSeconds) / 60 % 60
                     let seconds = Int(self.timeInSeconds) % 60
-                    thirdCell.label.text = String(format: "%02i:%02i:%02i:%02i", days, hours, minutes, seconds) + " until Demo Day..."
+                    thirdCell.timerLabel.text = String(format: "%02i:%02i:%02i:%02i", days, hours, minutes, seconds)
+                    thirdCell.assignmentLabel.text = " until Demo Day..."
                 }
                 
                 timer.fire()
                  //*/
-                thirdCell.label.font = UIFont(name: "Thirtysix", size: 36)
-                thirdCell.label.textColor = UIColor.weLearnGreen
-                thirdCell.label.textAlignment = .center
-                thirdCell.label.layer.shadowColor = UIColor.weLearnBlack.cgColor
-                thirdCell.label.layer.shadowOffset = CGSize(width: -2, height: 3)
-                thirdCell.label.layer.shadowOpacity = 3
-                thirdCell.label.layer.shadowRadius = 1
-                thirdCell.label.layer.masksToBounds = false
-                thirdCell.label.numberOfLines = 2
             }
             
         default:
