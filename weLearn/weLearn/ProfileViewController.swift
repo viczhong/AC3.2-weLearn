8883ed43f5ee22c5188c08867d64d71e1a228e68
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-03-07 17:12:45 -0500

    started profile vc, added profile icon asset

--- weLearn/weLearn/ProfileViewController.swift
+++ weLearn/weLearn/ProfileViewController.swift
@@ -7,20 +7,45 @@
 //
 
 import UIKit
+import SnapKit
 
 class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.weLearnLightGreen
+        
+        viewHeirarchy()
+        configureConstraints()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func viewHeirarchy() {
+        self.view.addSubview(profileBox)
+        self.view.addSubview(profilePic)
+        self.view.addSubview(nameLabel)
+        self.view.addSubview(emailLabel)
+        self.view.addSubview(classLabel)
+        self.view.addSubview(editButton)
+        self.view.addSubview(gradeBox)
+        self.view.addSubview(achievementBox)
     }
     
+    func configureConstraints() {
+        profileBox.snp.makeConstraints { view in
+            
+        }
+        
+        achievementBox.snp.makeConstraints { view in
+        
+        }
+        
+        gradeBox.snp.makeConstraints {view in
+        
+        }
+    }
 
     /*
     // MARK: - Navigation
@@ -31,5 +56,65 @@ class ProfileViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    lazy var profileBox: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.white
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOffset = CGSize(width: -2, height: 3)
+        view.layer.shadowOpacity = 0.5
+        view.layer.shadowRadius = 3
+        view.layer.masksToBounds = false
+        return view
+    }()
+    
+    lazy var profilePic: UIImageView = {
+        let pic = UIImageView()
+        pic.layer.borderColor = UIColor.black.cgColor
+        pic.layer.borderWidth = 5
+        return pic
+    }()
+    
+    lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    lazy var emailLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    lazy var classLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    lazy var editButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    lazy var achievementBox: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.white
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOffset = CGSize(width: -2, height: 3)
+        view.layer.shadowOpacity = 0.5
+        view.layer.shadowRadius = 3
+        view.layer.masksToBounds = false
+        return view
+    }()
+
+    lazy var gradeBox: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.white
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOffset = CGSize(width: -2, height: 3)
+        view.layer.shadowOpacity = 0.5
+        view.layer.shadowRadius = 3
+        view.layer.masksToBounds = false
+        return view
+    }()
 
 }
