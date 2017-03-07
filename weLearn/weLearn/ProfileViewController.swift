5b92ce9e0d5d5f550ef71c6341e7dbd5e428bc14
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-03-07 17:29:21 -0500

    added profile link in navbar of home

--- weLearn/weLearn/ProfileViewController.swift
+++ weLearn/weLearn/ProfileViewController.swift
@@ -14,6 +14,8 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.title = "Profile"
+        
         self.view.backgroundColor = UIColor.weLearnLightGreen
         
         viewHeirarchy()
