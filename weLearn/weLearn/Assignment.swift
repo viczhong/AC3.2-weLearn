f3cccbad350667eda8039d0722406e768447194f
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-03-08 16:10:30 -0500

    rudimentary assignment tableview started...needs a lot more

---
+++ weLearn/weLearn/Assignment.swift
@@ -0,0 +1,19 @@
+//
+//  Assignment.swift
+//  weLearn
+//
+//  Created by Marty Avedon on 3/8/17.
+//  Copyright © 2017 Victor Zhong. All rights reserved.
+//
+
+import Foundation
+
+class Assignment {
+    let date: String
+    let assignmentTitle: String
+    
+    init(date: String, assignmentTitle: String) {
+        self.date = date
+        self.assignmentTitle = assignmentTitle
+    }
+}
