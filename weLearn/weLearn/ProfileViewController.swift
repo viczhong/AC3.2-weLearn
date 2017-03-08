2de41fbb26709cb3b99d74b1c480a6dced72cb21
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-03-07 22:27:58 -0500

    further work refining profile page

--- weLearn/weLearn/ProfileViewController.swift
+++ weLearn/weLearn/ProfileViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 import SnapKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // we shall make cells for the acheievements and grades
+    // grades will be a simple cell
+    // achievements will be a cell containing...a collection view or scroll view
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         
         self.title = "Profile"
         
@@ -22,6 +29,10 @@ class ProfileViewController: UIViewController {
         configureConstraints()
         
         self.edgesForExtendedLayout = .bottom
+        
+        tableView.separatorStyle = .none
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 268.0
 
         // Do any additional setup after loading the view.
     }
@@ -33,13 +44,21 @@ class ProfileViewController: UIViewController {
         self.view.addSubview(emailLabel)
         self.view.addSubview(classLabel)
         self.view.addSubview(editButton)
-        self.view.addSubview(gradeBox)
-        self.view.addSubview(achievementBox)
+        self.view.addSubview(tableView)
+//        self.view.addSubview(achievementBox)
+//        self.view.addSubview(titleForAchievementsBox)
+//        self.view.addSubview(gradesBox)
+//        self.view.addSubview(titleForGradesBox)
     }
     
     func configureConstraints() {
+        tableView.snp.makeConstraints { (tV) in
+            tV.leading.trailing.bottom.equalToSuperview()
+            tV.top.equalTo(profileBox.snp.bottom)
+        }
+        
         profileBox.snp.makeConstraints { view in
-            view.top.equalToSuperview().offset(40)
+            view.top.equalToSuperview().offset(20)
             view.centerX.equalToSuperview()
             view.leading.equalToSuperview().offset(25)
             view.trailing.equalToSuperview().inset(25)
@@ -62,41 +81,95 @@ class ProfileViewController: UIViewController {
         }
         
         classLabel.snp.makeConstraints { view in
-            view.top.equalTo(emailLabel.snp.bottom).offset(10)
+            view.top.equalTo(emailLabel.snp.bottom).offset(5)
             view.trailing.equalTo(profileBox).inset(20)
             view.bottom.equalTo(profileBox).inset(20)
         }
         
-        achievementBox.snp.makeConstraints { view in
-            view.top.equalTo(profileBox.snp.bottom).offset(20)
-            view.centerX.equalToSuperview()
-            view.leading.equalToSuperview().offset(25)
-            view.trailing.equalToSuperview().inset(25)
-            view.height.equalTo(100)
+//        achievementBox.snp.makeConstraints { view in
+//            view.top.equalTo(profileBox.snp.bottom).offset(20)
+//            view.centerX.equalToSuperview()
+//            view.leading.equalToSuperview().offset(25)
+//            view.trailing.equalToSuperview().inset(25)
+//            view.height.equalTo(100)
+//        }
+//        
+//        titleForAchievementsBox.snp.makeConstraints { view in
+//            view.top.leading.equalTo(achievementBox).offset(20)
+//        }
+//        
+//        gradesBox.snp.makeConstraints {view in
+//            view.top.equalTo(achievementBox.snp.bottom).offset(20)
+//            view.centerX.equalToSuperview()
+//            view.leading.equalToSuperview().offset(25)
+//            view.trailing.equalToSuperview().inset(25)
+//            view.height.equalTo(150)
+//        }
+//        
+//        titleForGradesBox.snp.makeConstraints { view in
+//            view.top.leading.equalTo(gradesBox).offset(20)
+//        }
+    }
+    
+    // Mark: - Table view stuff
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Achievements"
+        case 1:
+           return "Grades"
+        default:
+            return ""
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            // Achievements
+            return 1
+        case 1:
+            // Grades
+            return 1 // we'll make an array to hold all of the grades later
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell = UITableViewCell()
+        
+        switch indexPath.section {
+        case 0:
+            return cell
+        case 1:
+            return cell
+        default:
+            break
         }
         
-        gradeBox.snp.makeConstraints {view in
-            view.top.equalTo(achievementBox.snp.bottom).offset(20)
-            view.centerX.equalToSuperview()
-            view.leading.equalToSuperview().offset(25)
-            view.trailing.equalToSuperview().inset(25)
-            view.height.equalTo(150)
-        }
+        cell.layer.shadowOffset = CGSize(width: 2, height: 3)
+        cell.layer.shadowColor = UIColor.black.cgColor
+        cell.layer.shadowRadius = 3
+        cell.layer.masksToBounds = false
+        
+        return cell
     }
+    
+        
+        
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    // Mark: - Views made here
     
     lazy var profileBox: UIView = {
         let view = UIView()
-        view.backgroundColor = UIColor.white
+        view.backgroundColor = UIColor.clear
         view.layer.shadowColor = UIColor.black.cgColor
         view.layer.shadowOffset = CGSize(width: -2, height: 3)
         view.layer.shadowOpacity = 0.5
@@ -115,22 +188,25 @@ class ProfileViewController: UIViewController {
     
     lazy var nameLabel: UILabel = {
         let label = UILabel()
+        label.backgroundColor = UIColor.weLearnCoolWhite
         label.text = "Karen Fuentes"
-        label.font = UIFont(name: "Avenir-LightOblique", size: 20)
+        label.font = UIFont(name: "Avenir-Light", size: 24)
         return label
     }()
     
     lazy var emailLabel: UILabel = {
         let label = UILabel()
+        label.backgroundColor = UIColor.weLearnCoolWhite
         label.text = "karen@karen.com"
-        label.font = UIFont(name: "Avenir-LightOblique", size: 20)
+        label.font = UIFont(name: "Avenir-LightOblique", size: 16)
         return label
     }()
     
     lazy var classLabel: UILabel = {
         let label = UILabel()
+        label.backgroundColor = UIColor.weLearnCoolWhite
         label.text = "AC3.2"
-        label.font = UIFont(name: "Avenir-LightOblique", size: 20)
+        label.font = UIFont(name: "Avenir-LightOblique", size: 16)
         return label
     }()
     
@@ -139,26 +215,47 @@ class ProfileViewController: UIViewController {
         return button
     }()
     
-    lazy var achievementBox: UIView = {
-        let view = UIView()
-        view.backgroundColor = UIColor.white
-        view.layer.shadowColor = UIColor.black.cgColor
-        view.layer.shadowOffset = CGSize(width: -2, height: 3)
-        view.layer.shadowOpacity = 0.5
-        view.layer.shadowRadius = 3
-        view.layer.masksToBounds = false
-        return view
-    }()
-
-    lazy var gradeBox: UIView = {
-        let view = UIView()
-        view.backgroundColor = UIColor.white
-        view.layer.shadowColor = UIColor.black.cgColor
-        view.layer.shadowOffset = CGSize(width: -2, height: 3)
-        view.layer.shadowOpacity = 0.5
-        view.layer.shadowRadius = 3
-        view.layer.masksToBounds = false
-        return view
+//    lazy var achievementBox: UIView = {
+//        let view = UIView()
+//        view.backgroundColor = UIColor.white
+//        view.layer.shadowColor = UIColor.black.cgColor
+//        view.layer.shadowOffset = CGSize(width: -2, height: 3)
+//        view.layer.shadowOpacity = 0.5
+//        view.layer.shadowRadius = 3
+//        view.layer.masksToBounds = false
+//        return view
+//    }()
+//    
+//    lazy var titleForAchievementsBox: UIView = {
+//        let label = UILabel()
+//        label.font = UIFont(name: "Avenir-Black", size: 20)
+//        label.text = "Achievements"
+//        return label
+//    }()
+//
+//    lazy var gradesBox: UIView = {
+//        let view = UIView()
+//        view.backgroundColor = UIColor.white
+//        view.layer.shadowColor = UIColor.black.cgColor
+//        view.layer.shadowOffset = CGSize(width: -2, height: 3)
+//        view.layer.shadowOpacity = 0.5
+//        view.layer.shadowRadius = 3
+//        view.layer.masksToBounds = false
+//        return view
+//    }()
+//    
+//    lazy var titleForGradesBox: UIView = {
+//        let label = UILabel()
+//        label.font = UIFont(name: "Avenir-Black", size: 20)
+//        label.text = "Grades"
+//        return label
+//    }()
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()//(frame: .zero, style: .grouped)
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
     }()
 
 }
