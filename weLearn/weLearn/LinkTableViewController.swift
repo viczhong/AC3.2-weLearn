7c0bd77b2e58ab656c3f978f8d28f8c8ecb0bead
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-03-09 15:52:29 -0500

    started going into guts of links tableview

--- weLearn/weLearn/LinkTableViewController.swift
+++ weLearn/weLearn/LinkTableViewController.swift
@@ -17,6 +17,10 @@ class LinksTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.title = "Links"
+        
+        // tableView.register(LinkTableViewCell.self, forCellReuseIdentifier: "LinkTableViewCell")
+        
         self.getDataInfo()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -50,12 +54,17 @@ class LinksTableViewController: UITableViewController {
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return links.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell = UITableViewCell()
     }
     
 }
